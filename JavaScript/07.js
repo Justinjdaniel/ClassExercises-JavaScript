026b68fdec5bf84126d4300c1dc94fc7f5f1db23
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2020-05-07 23:51:56 +0530

    Exercise 07 Added

---
+++ JavaScript/07.js
@@ -0,0 +1,16 @@
+function findLarge(){
+    var int1 = document.getElementById("integer1").value;
+    var int2 = document.getElementById("integer2").value;
+
+    var larger = int1 && int2;
+
+    if(larger && int1 === int2)
+        document.getElementById("solution07").innerHTML = "Both numbers are equal!";
+
+    else if(larger)
+        document.getElementById("solution07").innerHTML = "The larger number is " + Math.max(int1,int2);
+
+    else
+        document.getElementById("solution07").innerHTML = "Please add an integer!";
+
+}

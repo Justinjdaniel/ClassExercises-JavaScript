b6fef5ad6bc82482e4e03e0f1e42e0dc04ea8973
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2020-05-09 21:46:50 +0530

    Stable Version
    Tweaked Exercise 08
    Re-checked conditions and O/p Verified.

--- JavaScript/08.js
+++ JavaScript/08.js
@@ -2,20 +2,19 @@ var x = 0;
 var array = Array();
 
 function add(){
- array[x] = document.getElementById("object").value;
- document.getElementById("add").innerHTML = "Object: " + array[x] + " added at index " + x;
- x++;
- document.getElementById("object").value = "";
+    array[x] = document.getElementById("object").value;
+    document.getElementById("add").innerHTML = "Object : " + array[x] + " added ";
+    x++;
+    document.getElementById("object").value = "";
 }
 
 function display(){
-   var display = "<hr>";
-    
-   for (var y=0; y<array.length; y++)
-   {
-    display += "Object " + y + " = " + array[y] + "<br/>";
-   }
-   document.getElementById("display").innerHTML = display;
+    var display = "";
+
+    for(index in array){
+        display += array[index] + ", ";
+    }
+    document.getElementById("display").innerHTML = "<hr> Objects added are <br>" + display + ".";
 }
 
 function object_sort(){
